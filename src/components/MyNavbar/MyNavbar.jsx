8667c9fc3535770e6ca9378595208992c8891446
Francisco-Fortes <fortesfco@gmail.com>
2022-11-25 12:11:19 +0100

    Navbar added

---
+++ src/components/MyNavbar/MyNavbar.jsx
@@ -0,0 +1,40 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import "./my-navbar.css";
+// NavDropdown
+const MyNavbar = () => {
+  return (
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#">
+          <img src="../../public/assets/netflix_logo.png" alt="Netflix-logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">TV Shows</Nav.Link>
+            <Nav.Link href="#">
+              <img
+                src="../../public/assets/netflix_logo.png"
+                alt="Netflix-logo"
+              />
+            </Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
