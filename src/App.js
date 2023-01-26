5630a26c26c4546a204e6a25fa6e70afc206d643
Francisco-Fortes <fortesfco@gmail.com>
2023-01-26 22:50:48 +0100

    router-dom

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import "./App.css";
 import MyNavbar from "./components/MyNavbar/MyNavbar";
 // import CustomFooter from "./components/CustomFooter/CustomFooter";
 import MyCarousel from "./components/MyCarousel/MyCarousel";
+import { BrowserRouter, Routes } from "react-router-dom";
 // import MyFooter from "./components/MyFooter/MyFooter";
 function App() {
   return (
-    <div>
-      <MyNavbar />
-      <MyCarousel nameSaga="Star Wars" />
-      <MyCarousel nameSaga="BattleStar Galactica" />
-      <MyCarousel nameSaga="Lord of the Rings" />
-      {/* <CustomFooter /> */}
-      {/* <MyFooter /> */}
-    </div>
+    <BrowserRouter>
+      <div>
+        <MyNavbar />
+        <Routes>
+          <MyCarousel nameSaga="Star Wars" />
+          <MyCarousel nameSaga="BattleStar Galactica" />
+          <MyCarousel nameSaga="Lord of the Rings" />
+        </Routes>
+        {/* <CustomFooter /> */}
+        {/* <MyFooter /> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
